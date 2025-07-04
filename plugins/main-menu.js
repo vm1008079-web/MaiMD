d89dba7afaf44a413352ca1f4cce6f70b50fbc59
Ado <minexdt@gmail.com>
2025-07-04 17:27:23 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -585,7 +585,7 @@ let handler = async (m, { conn, args }) => {
 
 handler.help = ['menu']
 handler.tags = ['main']
-handler.command = ['menutest']
+handler.command = ['menu', 'menú' 'help']
 
 export default handler
 
