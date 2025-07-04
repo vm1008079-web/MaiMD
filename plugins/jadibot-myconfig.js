c44598e7546bd94a750e101f90d4f6f49587017f
Ado <minexdt@gmail.com>
2025-07-04 15:04:08 -0600

    Crear jadibot-myconfig.js

---
+++ plugins/jadibot-myconfig.js
@@ -0,0 +1,37 @@
+//Hecho por Ado github.com/Ado-rgb
+import fs from 'fs'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+  const senderNumber = m.sender.replace(/[^0-9]/g, '')
+  const botPath = path.join('./JadiBots', senderNumber)
+  const configPath = path.join(botPath, 'config.json')
+
+  if (!fs.existsSync(botPath) || !fs.existsSync(configPath)) {
+    return m.reply('✧ Este comando es sólo para sub-bots con sesión activa.')
+  }
+
+  try {
+    const config = JSON.parse(fs.readFileSync(configPath))
+    let txt = `✧ Configuración actual del Sub-Bot ✧\n\n`
+    txt += `• Nombre: *${config.name || 'Sin nombre'}*\n`
+    txt += `• Banner: ${config.banner || 'Sin banner'}\n\n`
+    txt += `Usa *#setname* para cambiar el nombre.\nUsa *#setbanner* para cambiar el banner.`
+
+    await conn.sendMessage(m.chat, {
+      image: { url: config.banner || global.banner },
+      caption: txt
+    }, { quoted: m })
+
+  } catch (e) {
+    console.error(e)
+    m.reply('❌ Error al leer la configuración.')
+  }
+}
+
+handler.help = ['myconfig']
+handler.tags = ['serbot']
+handler.command = /^myconfig$/i
+handler.owner = false
+
+export default handler
