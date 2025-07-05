0f2c3d8e7dcfe2f34b1caae80728c29685d42186
Ado <minexdt@gmail.com>
2025-07-05 14:49:42 -0600

    Actualizar grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -60,6 +60,5 @@ handler.help = ['kick']
 handler.tags = ['group']
 handler.command = ['kick','echar','hechar','sacar','ban']
 handler.group = true
-handler.botAdmin = true
 
 export default handler
